668294a58596d0fc0a4fe142a0288b738bbbf18d
lws <chenkun_lws@126.com>
2019-04-10 18:18:37 +0800

    Added print "Hello World!" to the screen

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
 
+use bootloader::entry_point;
+use bootloader::BootInfo;
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+entry_point!(kernel_main);
+
+static HELLO: &[u8] = b"Hello World!";
+
+fn kernel_main(_boot_info: &'static BootInfo) -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
+    halt()
 }
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    halt()
+}
+
+fn halt() -> ! {
     loop {}
 }
